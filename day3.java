e6a04423583f5982f539753d174e43dbc4c99d6a
Halik7 <khaliquzumaki25@gmail.com>
2024-09-04 15:32:16 +0800

    hari ke 3
    Membuat System.out.println

---
+++ day3.java
@@ -0,0 +1,10 @@
+package ISC; 
+
+public class KHALIQ {
+    public static void main (String[] arts){
+        System.out.println("nama : KHALIQ ");
+        System.out.println("nim  :D0224328");
+        System.out.println("alamat: pamboang");
+    }
+    }
+    
