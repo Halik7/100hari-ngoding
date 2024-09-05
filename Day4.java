e0242a73c963358a5a86af607194194c81f2d0b4
Halik7 <khaliquzumaki25@gmail.com>
2024-09-05 12:07:39 +0800

    Hari ke 4
    membuat print dan printf

--- Day4.java
+++ Day4.java
@@ -7,6 +7,6 @@ public class KHALIQ {
         System.out.print("kelas: D");
 System.out.printf("alamat : pamboang%d");
 System.out.printf("umur   : 17 tahun%d");
-System.out.printf(" TB    :171 cm%d);
+System.out.printf(" TB    :171 cm%d");
     }
     }
