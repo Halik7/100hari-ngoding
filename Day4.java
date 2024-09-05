63e05d3321f0bcacc7dd13f5549c1ab8992c659e
Halik7 <khaliquzumaki25@gmail.com>
2024-09-05 12:01:39 +0800

    Hari ke 4
    membuat print dan printf

---
+++ Day4.java
@@ -0,0 +1,12 @@
+package ISC; 
+
+public class KHALIQ {
+    public static void main (String[] arts){
+        System.out.print("nama : KHALIQ ");
+        System.out.print("nim  :D0224328");
+        System.out.print("kelas: D");
+System.out.printf("alamat : pamboang%d");
+System.out.printf("umur   : 17 tahun%d");
+System.out.printf(" TB    :171 cm%d);
+    }
+    }
