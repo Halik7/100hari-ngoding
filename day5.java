60857460cc846a3b453f03b092aa70350902dd3d
Halik7 <khaliquzumaki25@gmail.com>
2024-09-06 18:35:45 +0800

    Hari ke5
    Membuat komentar

---
+++ day5.java
@@ -0,0 +1,11 @@
+package ISC; 
+
+public class KHALIQ {
+    public static void main (String[] arts){
+        System.out.println("nama : KHALIQ ");
+        System.out.println("nim  :D0224328");
+        System.out.printf(" kelas:D%d ");
+/* komentar Java sangat populer dan banyak digunakan dalam berbagai industri. Banyak perusahaan besar dan proyek open-source menggunakan Java, sehingga ada banyak dukungan dan sumber daya untuk belajar dan memecahkan masalah.*/
+    }
+    }
+    
