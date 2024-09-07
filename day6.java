db03742b0effd2ce90a9933b8e3104bbe46d5ddf
Halik7 <khaliquzumaki25@gmail.com>
2024-09-07 16:30:38 +0800

    hari ke 6
    Membuat variabel deklarasi

--- day6.java
+++ day6.java
@@ -4,8 +4,8 @@ public class KHALIQ {
     
     public static void main(String[] args)public class Main{
         // Deklarasi variabel
-        int umur = 18;
-        String nama = "khaliq";
-String alamat = " pamboang ";
+    String nama;
+       int umur;
+  String alamat;
     }
 }
