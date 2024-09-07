76713d256e8fb2f2047857c20f1c05d5bbeab925
Halik7 <khaliquzumaki25@gmail.com>
2024-09-07 15:50:24 +0800

    hari ke 6
    Membuat variabel : deklarasi

---
+++ day6.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class KHALIQ {
+    
+    public static void main(String[] args)public class Main{
+        // Deklarasi variabel
+        int umur = 18;
+        String nama = "khaliq";
+
+        // Mencetak nilai variabel
+        System.out.println("Nama: " + khaliq);
+        System.out.println("umur: " + 18);
+    }
+}
