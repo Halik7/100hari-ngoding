ecedb4fde42c6653489f0ea745e4c783cf99a78f
Halik7 <khaliquzumaki25@gmail.com>
2024-09-09 19:31:21 +0800

    Hari ke8
    membuat variabel update

---
+++ day8.java
@@ -0,0 +1,19 @@
+package isc;
+
+public class KHALIQ {
+   public static void main(String[] args) {
+        // Deklarasi variabel dan inisialisasi
+        String nama = khaliq;
+        int umur    = 18
+        // Menampilkan nilai awal
+        System.out.println("nama: " + nama);
+        System.out.println("umur: " + umur
+        // Meng-update nilai variabel
+        nama = khaliq;
+        umur = 18;
+        // Menampilkan nilai setelah update
+        System.out.println("nama: " + nama);
+        System.out.println("umur: " +umur) ;
+
+    }
+}
