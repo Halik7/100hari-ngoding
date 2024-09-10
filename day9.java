e776dd5dbf10ad144bd7aac01e47a2f41b717e87
Halik7 <khaliquzumaki25@gmail.com>
2024-09-10 20:28:58 +0800

    Hari ke9
    Membuat constanta Final variable

---
+++ day9.java
@@ -0,0 +1,18 @@
+package isc;
+
+public class KHALIQ {
+  
+   public static void main(String[] args) {
+   // constanta (Final variable) 
+   final string nama, kelas, mentor;
+   int hari;
+
+   nama  = "Khaliq";
+   kelas = "Informatika D";
+   mentor= "kak armawan";
+   hari  = 9;
+
+   System.out.printf("nama   :%s \n", nama) ;
+   System.out.printf("kelas  :%s \n", kelas) ;
+   System.out.printf("mentor :%s \n", mentor) ;
+   System.out.printf(" progres:hari ke%d \n", hari) ; 
