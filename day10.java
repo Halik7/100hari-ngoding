e974a6be0a092e479aceb5e5b8f16101cf4ce1ad
Halik7 <khaliquzumaki25@gmail.com>
2024-09-11 20:45:16 +0800

    Hari ke10
    Membuat tipe data byte

---
+++ day10.java
@@ -0,0 +1,25 @@
+package isc;
+
+public class KHALIQ {
+    
+    public static void main(String[] args) {
+   // tipe data byte
+
+    nilai byte angka
+    byte angka1=10 +;
+         angka2=-10 -;
+         angak3=15 *;
+
+   
+
+
+    hasil byte angka 
+    System.out printf=(byte)"perjumlahan angka1+perjumlahan) ;
+    System.out printf=(byte)"pengurangan angka2+pengurangan) ;
+    System.out printf=(byte)"perkalian angka3+perkalian) ;
+
+
+
+    }
+    
+}
