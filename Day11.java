6b5ce00138db03c488503f4fa88b5cd3e9a6884b
Halik7 <khaliquzumaki25@gmail.com>
2024-09-12 18:00:56 +0800

    Hari ke11
    Membuat tipe data short

---
+++ Day11.java
@@ -0,0 +1,13 @@
+package day11;
+
+public class Main {
+    
+    public static void main(String[] args) {
+      // tipe data short
+        short jumlah = 100;
+        
+    System.out.println ("jumlah alam semesta " + jumlah);
+        
+    }
+    
+}
