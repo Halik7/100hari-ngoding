d1c64fd31d398d83685373a6e4df197ddd2ee32d
Halik7 <khaliquzumaki25@gmail.com>
2024-09-13 17:05:28 +0800

    Tipe data int
    membuat tipe data int

---
+++ day12.java
@@ -0,0 +1,11 @@
+package mentoring_armawan;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+         //tipe data int
+        int hargabaju = 150;
+        System.out.println("harga baju " + hargabaju);
+    }
+}
