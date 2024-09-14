730f010b44be420e635be27a5865b3edf1e277bb
Halik7 <khaliquzumaki25@gmail.com>
2024-09-14 20:19:37 +0800

    Evaluasi
    membuat 3 inputan tipe data byte, short, dan int kemudian memanggil variabelnya menggunakan System.out.println

---
+++ day13.java
@@ -0,0 +1,16 @@
+package evaluasi;
+
+public class day13 {
+    
+    public static void main(String[] args) {
+        
+        byte a = 20;
+        short b = 24;
+        int c = 2024;
+        
+        System.out.println("hasil tipe data byte \t= "+a);
+        System.out.println("hasil tipe data short\t= "+b);
+        System.out.println("hasil tipe data int\t= "+c);
+    }
+    
+}
