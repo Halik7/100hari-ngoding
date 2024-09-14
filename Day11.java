b5b208d8ff2b4e0e47f4888c3239cdac56619fa2
Halik7 <khaliquzumaki25@gmail.com>
2024-09-14 13:33:11 +0800

    Tipe data short
    Membuat tipe data short

--- Day11.java
+++ Day11.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
       // tipe data short
         short jumlah = 100;
-        
-    System.out.println ("jumlah alam semesta " + jumlah);
+      // tipe data short adalah untuk bilang bulat dari -32768 sampai 32767 
+    System.out.println ("jumlah " + jumlah);
         
     }
     
