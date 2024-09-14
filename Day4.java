8727f5a059d38d3117acca511e0e1245a5eccfc0
Halik7 <khaliquzumaki25@gmail.com>
2024-09-14 13:03:45 +0800

    System.out.print dan System.out.printf
    Membuat System.out.print dan System.out.printf

--- Day4.java
+++ Day4.java
@@ -1,12 +1,15 @@
-package ISC; 
+package mentoring_armawan;
 
-public class KHALIQ {
-    public static void main (String[] arts){
+public class day4 {
+    
+    public static void main(String[] args) {
+        
         System.out.print("nama : KHALIQ ");
-        System.out.print("nim  :D0224328");
-        System.out.print("kelas: D");
-System.out.printf("alamat : pamboang%d");
-System.out.printf("umur   : 17 tahun%d");
-System.out.printf(" TB    :171 cm%d");
+        System.out.print("nim  :D0224328 ");
+        System.out.print("kelas: D ");
+System.out.printf("alamat : pamboang ");
+System.out.printf("umur   : 17 tahun ");
+System.out.printf(" TB    :171 cm ");
     }
+    
     }
