359dfce337d479b428682d4b8ef5dd2792a17d11
Halik7 <khaliquzumaki25@gmail.com>
2024-09-15 17:26:15 +0800

    Tipe data long
    Membuat tipe data long kemudian memanggil nya dengan System.out.println

---
+++ day14.java
@@ -0,0 +1,12 @@
+package mentor_armawan;
+
+public class day14 {
+    
+    public static void main(String[] args) {
+        //tipe data long
+        long largeNumber = 123456789012345L; // Menambahkan 'L' di akhir angka untuk menunjukkan bahwa ini adalah tipe data long
+        System.out.println("Nilai dari largeNumber adalah: " + largeNumber);
+
+    }
+    
+}
