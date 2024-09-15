ead844f98837ca90d3800e5c3288915de9a4f700
Halik7 <khaliquzumaki25@gmail.com>
2024-09-15 18:51:28 +0800

    Tipe data long
    Membuat tipe data long kemudian mencari hasil dengan System.out.println

--- day14.java
+++ day14.java
@@ -4,8 +4,8 @@ public class day14 {
     
     public static void main(String[] args) {
         //tipe data long
-        long largeNumber = 123456789012345L; // Menambahkan 'L' di akhir angka untuk menunjukkan bahwa ini adalah tipe data long
-        System.out.println("Nilai dari largeNumber adalah: " + largeNumber);
+        long jumlah = 2025L; // Menambahkan 'L' di akhir angka untuk menunjukkan bahwa ini adalah tipe data long
+        System.out.println("Nilai dari jumlah adalah: " + jumlah);
 
     }
     
