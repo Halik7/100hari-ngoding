72be54b9e7294f1b21b2ffd4f09b814e8b61f2dd
Halik7 <khaliquzumaki25@gmail.com>
2024-09-16 20:21:29 +0800

    Tipe data double
    Membuat tipe data double kemudian memanggil nya dengan System.out.println

---
+++ day15.java
@@ -0,0 +1,16 @@
+package mentoring_armawan;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+       //tipe data double adalah tipe untuk menyimpan bilangan  desimal atau angka pecahan atau angka koma
+        
+        double angkaDesimal1 = 10.5;
+        double angkaDesimal2 = 3.14;
+        
+        System.out.println("Nilai 1: " + angkaDesimal1);
+        System.out.println("Nilai 2: " + angkaDesimal2);
+        
+    }
+}
