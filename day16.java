ec9bab88e034c8c04162a99fd20d8c9ef9817bce
Halik7 <khaliquzumaki25@gmail.com>
2024-09-17 19:47:24 +0800

    Tipe data float
    Membuat tipe data float

---
+++ day16.java
@@ -0,0 +1,15 @@
+package mentor_armawan;
+
+public class day16 {
+    
+    public static void main(String[] args) {
+        
+        //tipe data float adalah tipe data bilangan desimal atau angka pecahan atau lebih jelasnya angka yang memiliki koma
+        
+        float angkaDesimal = 13.4f;
+        
+        System.out.println("Nilai angkaDesimal "+ angkaDesimal);
+        
+    }
+    
+}
