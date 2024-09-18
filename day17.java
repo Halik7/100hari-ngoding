6016c985af3c94ac3bb3bf88c2a9bc51964012b8
Halik7 <khaliquzumaki25@gmail.com>
2024-09-18 21:38:39 +0800

    Tipe data char
    Membuat tipe data char lalu memanggilnya dengan System.out.print

---
+++ day17.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+    public class day17 {
+    
+    public static void main(String[] args) {
+        
+        //tipe data char
+         char nilai ='1';
+        System.out.print("hasil :"+nilai);
+        
+    }
+    
+}
