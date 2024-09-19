009afe44730f1af6707502b7ac4a81efabc63883
Halik7 <khaliquzumaki25@gmail.com>
2024-09-19 22:20:18 +0800

    Tipe data boolean
    membuat tipe data boolean

---
+++ day18.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+    public class day18 {
+    
+    public static void main(String[] args) {
+        
+     //tipe data boolean,Tipe data boolean adalah tipe data yang hanya memiliki dua nilai, yaitu benar (true) atau salah (false) 
+        boolean statusMaba = true;
+        
+     System.out.println("apakah kamu maba? :"+statusMaba);
+      
+    }
+     
+}
