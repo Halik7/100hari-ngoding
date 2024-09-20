890f4c063291633e1acb715e9bd4019c40981de5
Halik7 <khaliquzumaki25@gmail.com>
2024-09-20 21:35:01 +0800

    Tipe data reference String
    Membuat tipe data reference String

---
+++ day19.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class Day19 {
+    
+    public static void main(String[] args) {
+        
+       String alamat1 = "pamboang";
+       String alamat2 = alamat1;
+        
+        System.out.println("alamat1 : "+alamat1);
+        System.out.println("alamat2 : "+alamat2);
+    }
+    
+}
