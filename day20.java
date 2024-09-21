1819eed029a39ebc5b5a65082154ea88ac6d3129
Halik7 <khaliquzumaki25@gmail.com>
2024-09-21 21:13:04 +0800

    Tipe data reference Byte
    Membuat tipe data reference Byte

---
+++ day20.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class day20 {
+    
+    public static void main(String[] args) {
+        
+      //  tipe data reference Byte
+        
+      Byte angka =100;
+        
+          System.out.println("saya punya "+ angka +" liter air");
+    }
+    
+}
