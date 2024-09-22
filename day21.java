df69c597515acaa975745e7d0400a52f8765409d
Halik7 <khaliquzumaki25@gmail.com>
2024-09-22 23:37:25 +0800

    tipe data reference Short
    Membuat tipe data reference Short

---
+++ day21.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class day21 {
+    
+    public static void main(String[] args) {
+        
+        //tipe data reference Short
+         Short uang = 30000;
+        
+        System.out.println("saya memiliki uang = Rp. "+ uang );
+    }
+    
+}
