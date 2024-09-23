bdd559f52d62d6e23e7631e4020367ddddc7ce68
Halik7 <khaliquzumaki25@gmail.com>
2024-09-23 23:45:08 +0800

    Tipe data reference Integer
    Membuat tipe data reference Integer

---
+++ day22.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class day22 {
+    
+    public static void main(String[] args) {
+       //tipe data reference Integer
+      
+      Integer ukuran =  41;
+      System.out.println("ukuran sepatu saya : "+ukuran );
+        
+    }
+    
+}
