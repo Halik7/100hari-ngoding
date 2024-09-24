399a6c704a8040fb8c45e1c6f1cabf52d1360ee2
Halik7 <khaliquzumaki25@gmail.com>
2024-09-24 20:40:30 +0800

    Tipe data reference Long
    Membuat tipe data reference Long

---
+++ day23.java
@@ -0,0 +1,12 @@
+package mentor_armawan;
+
+public class day23 {
+    
+    public static void main(String[] args) {
+        //tipe data reference Long
+        Long jumlah = 7777L;
+        
+        System.out.println("jumlah : "+  jumlah);
+    }
+    
+}
