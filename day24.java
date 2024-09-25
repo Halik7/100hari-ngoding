7359fa16206f5dfc0489089f6fc771111dc3b21d
Halik7 <khaliquzumaki25@gmail.com>
2024-09-25 17:20:46 +0800

    tipe data reference Float
    Membuat tipe data reference Float

---
+++ day24.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class Day24 {
+    
+    public static void main(String[] args) {
+        
+      //  tipe data reference Float
+      
+        Float angka = 17.7F;
+        System.out.println("angka :"+angka);
+    }
+    
+}
