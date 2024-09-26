54c486d033e7cef43a83dd444e27c514abc12132
Halik7 <khaliquzumaki25@gmail.com>
2024-09-26 22:27:27 +0800

    Tipe data reference Double
    Membuat tipe data reference Double

---
+++ day25.java
@@ -0,0 +1,15 @@
+package Mentor_armawan;
+
+public class day25 {
+    
+    public static void main(String[] args) {
+        
+        //tipe data reference Double
+         
+          Double angkaDesimal = 27.7;
+        
+        
+        System.out.println("angka desimal : "+ angkaDesimal);
+    }
+    
+}
