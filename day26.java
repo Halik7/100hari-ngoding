ecb3b2d9cc5909caa6870cf3b606ec01621a1cd7
Halik7 <khaliquzumaki25@gmail.com>
2024-09-27 21:41:34 +0800

    Tipe data reference Character
    Membuat tipe data reference Character

---
+++ day26.java
@@ -0,0 +1,12 @@
+package mentor_armawan;
+
+public class day26 {
+    
+    public static void main(String[] args) {
+        //tipe data reference Character
+         Character Huruf = 'H';
+        
+        System.out.println("Huruf : "+Huruf);
+    }
+    
+}
