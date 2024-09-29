3db18ed452949ad101e8012a971afd0d6a7a499b
Halik7 <khaliquzumaki25@gmail.com>
2024-09-29 20:54:44 +0800

    tipe data reference Boolean
    Membuat tipe data reference Boolean

---
+++ day28.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class day28 {
+    
+    public static void main(String[] args) {
+        
+        Boolean a = true;
+        Boolean b = false;
+        
+        System.out.println("saya maba = "+a);
+        System.out.println("saya bukan maba = "+b);
+    }
+    
+}
