12f743f253c6006b6d799cf031c599566dce9388
Halik7 <khaliquzumaki25@gmail.com>
2024-09-30 23:26:04 +0800

    Tipe data reference BigInteger
    Membuat tipe data reference BigInteger

---
+++ day29.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+import java.math.BigInteger;
+ class day29 {
+    
+    public static void main(String[] args) {
+         //Tipe data BigInteger adalah tipe data yang digunakan untuk menyimpan bilangan bulat yang sangat besar, yang tidak dapat ditampung oleh tipe data primitif seperti int atau long di Java dan beberapa bahasa pemrograman lainnya.
+        
+        BigInteger angka =new BigInteger ("999999999999");
+        
+        System.out.println("berapa jumlah manusia seluruh dunia "+angka);
+    }
+    
+}
