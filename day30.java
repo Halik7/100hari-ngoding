863517ec519ee0333afa7a1250f28632de5a574a
Halik7 <khaliquzumaki25@gmail.com>
2024-10-01 22:10:16 +0800

    tipe data reference BigDecimal
    Membuat tipe data reference BigDecimal

--- day30.java
+++ day30.java
@@ -4,8 +4,7 @@ public class day30 {
     
     public static void main(String[] args) {
         
-        //Tipe data reference BigDecimal adalah salah satu kelas di Java yang digunakan untuk merepresentasikan angka desimal dengan presisi tinggi,yang tidak bisa dilakukan dengan tipe data primitif seperti double atau float.
-        
+        //Tipe data reference BigDecimal 
         BigDecimal angka1 =new BigDecimal("0.05") ;
         BigDecimal angka2 =new BigDecimal("0.02") ;
         
