03efe4bb54bcd470a5fdc95dfc7dfba5c122767b
Halik7 <khaliquzumaki25@gmail.com>
2024-10-02 20:24:31 +0800

    Konversi String ke tipa data primitif
    Membuat konversi String ke tipe data primitif

---
+++ day31.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class day31 {
+    
+    public static void main(String[] args) {
+         //mengonversi String ke tipe data primitif
+        String str = "123";
+        int angka = Integer.parseInt(str);
+        
+        System.out.println("hasil konversi "+angka);
+    }
+    
+}
