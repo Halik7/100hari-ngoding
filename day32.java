67d0009e9a2dcc6e2659cef06f32fcc3f66a53f2
Halik7 <khaliquzumaki25@gmail.com>
2024-10-04 00:42:26 +0800

    Konversi data primitif ke String
    Membuat konversi data primitif ke String

---
+++ day32.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class day32 {
+    
+    public static void main(String[] args) {
+        
+       // konversi data primitif ke String
+        
+        Double angka = 77.7;
+        String desimal = String.valueOf(angka);
+        System.out.println("Double ke String :"+desimal);
+    }
+    
+}
