03ef34d6db5c8ee5ed3cc03623e6dcc43193a858
Halik7 <khaliquzumaki25@gmail.com>
2024-10-04 22:22:41 +0800

    Type casting
    Membuat type casting

---
+++ day33.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class day33 {
+    
+    public static void main(String[] args) {
+        
+        //type casting
+        int a =17;
+        float b = (float)a;
+        System.out.println("Hasil casting "+b);
+    }
+    
+}
