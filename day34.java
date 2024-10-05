01ef4a5c5154040154cffc2de72a9eccd955973d
Halik7 <khaliquzumaki25@gmail.com>
2024-10-05 21:11:27 +0800

    Input dari keyboard
    Membuat objek scanner untuk mengambil input dari keyboard

---
+++ day34.java
@@ -0,0 +1,24 @@
+package mentor_armawan;
+import java.util.Scanner;
+public class day34 {
+    
+    public static void main(String[] args) {
+        
+        // Membuat objek Scanner untuk mengambil input dari keyboard
+        Scanner scanner = new Scanner(System.in);
+
+        // Mengambil input berupa string
+        System.out.print("Masukkan nama Anda: ");
+        String nama = scanner.nextLine();
+
+        // Mengambil input berupa integer
+        System.out.print("Masukkan usia Anda: ");
+        int usia = scanner.nextInt();
+
+        // Menampilkan input yang telah diambil
+        System.out.println("Nama Anda adalah: " + nama);
+        System.out.println("Usia Anda adalah: " + usia + " tahun");
+
+    }
+    
+}
