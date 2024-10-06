4e4d9e9e5025f9e63ec3b52c5233d68310e81bef
Halik7 <khaliquzumaki25@gmail.com>
2024-10-06 22:57:48 +0800

    Operator aritmatika
    Membuat operator aritmatika penjumlahan dan pengurangan

--- day35.java
+++ day35.java
@@ -1,22 +1,17 @@
 package mentor_armawan;
-import java.util.Scanner;
+
 public class day35 {
     
     public static void main(String[] args) {
         
-        // Aritmatika perjumlahan dan pengurangan
-         Scanner input = new Scanner (System.in);
-        System.out.print("masukkan angka :");
-        int angka1 = input.nextInt();
-        System.out.print("masukkan angka :");
-        int angka2 = input.nextInt();
-        int hasilPenjumlahan = angka1 + angka2 ;
-        int hasilPengurangan = angka1 - angka2 ;
-        System.out.println("hasil penjumlahan :"+hasilPerjumlahan);
-        System.out.println("hasil pengurangan :"+hasilPengurangan);
-        
-        input.close();
+        // operator Aritmatika 
+          int a = 25;
+          int b = 15;
+        // penjumlahan dan pengurangan
+          int tambah = a+b;
+          int kurang = a-b;
+        System.out.println("hasil penjumlahan :"+tambah);
+        System.out.println("hasil pengurangan :"+kurang);
         
     }
-    
 }
