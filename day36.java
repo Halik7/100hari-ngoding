ac9ab5900e15f725f68a53f467853fd9eec30739
Halik7 <khaliquzumaki25@gmail.com>
2024-10-07 22:56:16 +0800

    operator aritmatika
    Membuat operator aritmatika perkalian, pembagian, dan mudolo

---
+++ day36.java
@@ -0,0 +1,16 @@
+package mantor_armawan;
+
+public class day36 {
+    
+    public static void main(String[] args) {
+        
+        //operator aritmatika perkalian,pembagian dan modulo
+         int a = 10;
+         int b = 2;
+        
+        System.out.println(a*b);//operator perkalian
+        System.out.println(a/b);//operator pembagian
+        System.out.println(a%b);//operator mudolo
+    }
+    
+}
