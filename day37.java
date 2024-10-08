e5ee584b1be06e0c69e21738bc71350befff267a
Halik7 <khaliquzumaki25@gmail.com>
2024-10-08 22:01:35 +0800

    Operator penugasan
    membuat pengisian nilai, pengisian penambahan, dan pengisian pengurangan

---
+++ day37.java
@@ -0,0 +1,21 @@
+package mentor_armawan;
+
+public class day37 {
+    
+    public static void main(String[] args) {
+        //operator penugasan 
+          
+          //pengisian nilai "="
+           int nilai = 7;
+          System.out.println(nilai);
+        //pengisian penambahan "+="
+         int a =5;
+             a +=3;
+        System.out.println(a);
+        //pengisian pengurangan "-="
+         int b =13;
+             b -=4;
+        System.out.println(b);
+    }
+    
+}
