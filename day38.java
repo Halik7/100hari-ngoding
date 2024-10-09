8c79e6e47f93e371efcfc762314ee8bd61d16c14
Halik7 <khaliquzumaki25@gmail.com>
2024-10-09 21:15:57 +0800

    Operator penugasan
    Mambuat operator penugasan, pengisian dan perkalian, pengisian dan pembagian

---
+++ day38.java
@@ -0,0 +1,17 @@
+package mentor_armawan;
+
+public class day38 {
+    
+    public static void main(String[] args) {
+        
+        //pengisian perkalian "*="
+          int a = 7;
+              a *=7;
+        System.out.println("hasil perkalian :"+a);
+        //pengisian pembagian "/="
+          int b = 21;
+              b /= 7;
+        System.out.println("hasil pembagian :"+b);
+    }
+    
+}
