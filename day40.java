0b23bf817d2b35dbb18f4a523d71387896f76a4c
Halik7 <khaliquzumaki25@gmail.com>
2024-10-11 21:31:33 +0800

    Operator perbandingan
    Membuat operator perbandingan lebih besar dan lebih kecil

--- day40.java
+++ day40.java
@@ -3,12 +3,11 @@ package mentor_armawan;
 public class day40 {
     
     public static void main(String[] args) {
-        //operator perbandingan
-        //lebih besar ">"
+        //operator perbandingan lebih besar dan lebih kecil
         int a =3;
         int b =1;
-        System.out.println(a > b);
-        System.out.println(a < b);
+        System.out.println(a > b);//lebih besar
+        System.out.println(a < b);//lebih kecil
     }
     
 }
