1cda1d2fb97d9e5d377ee092960dbedeb732a415
Halik7 <khaliquzumaki25@gmail.com>
2024-10-13 21:28:41 +0800

    Operator perbandingan
    Membuat operator perbandingan sama dengan dan tidak sama dengan

--- day42.java
+++ day42.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 public class day42 {
     
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
         
-       //operator perbandingan 
-        System.out.print("masukkan nilai : ");
-        int A = 7;
-            A = 7;
-        System.out.println(A==A);//operator sama dengan
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai A: ");
+        int A = input.nextInt();
+        System.out.print("Masukkan nilai B: ");
+        int B = input.nextInt();
         
-        System.out.print("masukkan nilai : ");
-        int B = 7;
-            B = 3;
-        System.out.println(B!=B);//operator tidak sama dengan
+        //operator perbandingan
+        System.out.println(A == B); // Membandingkan nilai A dan B
+        System.out.println(A != B); // Membandingkan nilai A dan B
+        
     }
-    
 }
