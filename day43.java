288d4dfc81b7e47bdd7e82dc2728fb83ae0ccc34
Halik7 <khaliquzumaki25@gmail.com>
2024-10-14 18:57:04 +0800

    Operator perbandingan
    Membuat operator perbandingan lebih besar sama dengan dan lebih kecil sama dengan

---
+++ day43.java
@@ -0,0 +1,16 @@
+package mentor_armawan;
+import java.util.Scanner;
+public class Day43 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai a: ");
+        int a = input.nextInt();
+        System.out.print("Masukkan nilai b: ");
+        int b = input.nextInt();
+        
+        System.out.println(a>=b);//lebih besar sama dengan
+        System.out.println(a<=b);//lebih kecil sama dengan
+    }
+    
+}
