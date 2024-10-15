c2d7255263bdbf8fa66119a67b37bff7c5a8f55d
Halik7 <khaliquzumaki25@gmail.com>
2024-10-15 20:52:42 +0800

    Operator logika
    Membuat operator logika AND atau DAN

---
+++ day44.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+import java.util.Scanner;
+public class day44 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+          boolean x = 7 >=5;
+          boolean y = 7 >=7;
+        System.out.println(y && x);//operator AND atau DAN
+        System.out.println(x && y);//operator AND atau DAN
+    }
+    
+}
