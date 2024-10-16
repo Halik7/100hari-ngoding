f094a28eb6c4458313262544510a0824bd036098
Halik7 <khaliquzumaki25@gmail.com>
2024-10-16 23:06:30 +0800

    Operator logika
    Membuat operator OR

---
+++ day45.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class day45 {
+    
+    public static void main(String[] args) {
+        //operator OR (atau)
+          boolean x = 8 >=5;
+          boolean y = 7 >=10;
+        System.out.println(x || y);
+        System.out.println(y || x);
+    }
+    
+}
