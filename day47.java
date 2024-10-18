7e59095b19206683cdd6c03487dfebd1c53a9e87
Halik7 <khaliquzumaki25@gmail.com>
2024-10-18 20:24:28 +0800

    Operator percabangan
    Membuat percabangan if

---
+++ day47.java
@@ -0,0 +1,18 @@
+package mentor_armawan;
+import java.util.Scanner;
+public class day47 {
+    
+    public static void main(String[] args) {
+       //percabangan if
+        Scanner a= new Scanner(System.in);
+        System.out.print("masukkan angka : ");
+        int h = a.nextInt();
+        
+        
+        if(h > 0) {
+        System.out.println("angka adalah positif");
+        }     
+    }
+        
+    
+}
