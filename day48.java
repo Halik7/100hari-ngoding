7ee246acb023cc0017e545d4f4f44b8de4804749
Halik7 <khaliquzumaki25@gmail.com>
2024-10-19 20:49:35 +0800

    Percabangan
    Percabangan if else

---
+++ day48.java
@@ -0,0 +1,18 @@
+package mentor_armawan;
+import java.util.Scanner;
+public class day48 {
+    
+    public static void main(String[] args) {
+        Scanner a = new Scanner (System.in);
+        System.out.print("masukkan angka : ");
+        int b = a.nextInt();
+        
+        if(b == 10 ){
+            System.out.println("positif");
+        }
+        else{
+        System.out.println("negatif");
+    }
+    }
+    
+}
