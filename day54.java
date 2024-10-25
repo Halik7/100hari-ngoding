b3afe31bef49d7f951e9f8b1d81a8367b917b0d4
Halik7 <khaliquzumaki25@gmail.com>
2024-10-25 22:23:21 +0800

    looping for
    Membuat looping for increment

---
+++ day54.java
@@ -0,0 +1,15 @@
+package mentor_armawan;
+import java.util.Scanner;
+public class day54 {
+    
+    public static void main(String[] args) {
+        
+         Scanner a = new Scanner (System.in);
+         System.out.print("masukkan angka : ");
+        int angka = a.nextInt();
+        // Looping for
+        for (int i = 1; i <= angka; i++) {
+            System.out.println("iterasi ke - "  + i);
+        }
+    }
+}
