9077f5d1d28baa8bd63c9da463122c63dd39ad01
Halik7 <khaliquzumaki25@gmail.com>
2024-10-29 23:36:14 +0800

    Looping while
    Membuat looping while decrement

---
+++ day58.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class day58 {
+    
+    public static void main(String[] args) {
+        int i = 5; // Inisialisasi variabel
+
+        while (i > 0) { // Kondisi loop
+            System.out.println("Nilai i: " + i);
+            i--; // Decrement
+        }
+    }
+}
