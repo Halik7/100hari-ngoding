7399bdab5dc1a9639d7b29b34cf2c47388f9a902
Halik7 <khaliquzumaki25@gmail.com>
2024-10-29 23:37:19 +0800

    Looping while
     Membuat looping while Increment

---
+++ day57.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class day57 {
+    
+    public static void main(String[] args) {
+        int i = 0; // Inisialisasi variabel
+
+        while (i < 5) { // Kondisi loop
+            System.out.println("Nilai i: " + i);
+            i++; // increment
+        
+        }
+    }
+}
