baa6926f0ea1dcf49c65107d1470942909f663d0
Halik7 <khaliquzumaki25@gmail.com>
2024-10-30 22:46:54 +0800

    Looping for each
    Membuat looping for each

---
+++ day59.java
@@ -0,0 +1,12 @@
+package mentor_armawan;
+
+public class day59 {
+    
+    public static void main(String[] args) {
+        
+         String[] nama = {"H", "A", "L", "I", "K"};
+        for (String b : nama) {
+        System.out.print(b+" ");
+        }
+    }
+}
