019c19470814ed453f3e7364b887f5e653b523a4
Halik7 <khaliquzumaki25@gmail.com>
2024-10-31 22:59:59 +0800

    Looping do while
    Membuat looping do while

---
+++ day60.java
@@ -0,0 +1,12 @@
+package mentor_armawan;
+
+public class day60 {
+    
+    public static void main(String[] args) {
+        int i = 1; // Inisialisasi variabel i
+        do {
+            System.out.println("Perulangan do while ke-" + i); // Menampilkan nilai i
+            i++; // Increment i
+        } while (i <= 5); // Kondisi untuk melanjutkan perulangan
+    }
+}
