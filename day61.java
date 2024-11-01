dbc2285846dfed77e0852a1ed1e29765d72b093a
Halik7 <khaliquzumaki25@gmail.com>
2024-11-01 21:19:19 +0800

    Infinite loop
    Membuat Infinite loop

---
+++ day61.java
@@ -0,0 +1,10 @@
+package mentor_armawan;
+
+public class day61 {
+    
+    public static void main(String[] args) {
+        while(true){
+        System.out.println("ini adalah infinite loop. ");
+        }
+    }
+}
