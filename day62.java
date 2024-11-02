65bbbabafd3a22c28f1dfbda9965459569579ccc
Halik7 <khaliquzumaki25@gmail.com>
2024-11-02 23:27:13 +0800

    Keyword break
    Membuat keyword break

---
+++ day62.java
@@ -0,0 +1,15 @@
+package mentor_armawan;
+
+public class day62 {
+    
+    public static void main(String[] args) {
+         int i = 0;
+        while(true){
+            i++;
+            System.out.println(i);
+        
+        if(i == 8 )
+            break;
+        }
+    }
+}
