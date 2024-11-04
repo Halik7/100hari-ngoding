b063ec932859540c50c76e0ea1522452f6f86e81
Halik7 <khaliquzumaki25@gmail.com>
2024-11-04 23:50:12 +0800

    Break label
    Membuat break label

--- day64.java
+++ day64.java
@@ -4,14 +4,14 @@ public class day64 {
     
     public static void main(String[] args) {
         
-        luarLoop: // Label untuk outer loop
+        outerLoop: // Label untuk luar loop
         for (int i = 1; i <= 5; i++) {
             System.out.println("iterasi luar loop : " + i);
             for (int j = 1; j <= 5; j++) {
                 System.out.println("iterasi dalam loop : " + j);
                 // Jika j sama dengan 3, keluar dari outer loop
                 if (j == 3) {
-                    break luarLoop; // Menghentikan eksekusi dari outer loop
+                    break outerLoop; // Menghentikan eksekusi dari outer loop
                 }
             }
         }
