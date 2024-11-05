e3f6bc7befd048332b79b1e00b0c9205de999839
Halik7 <khaliquzumaki25@gmail.com>
2024-11-05 22:39:46 +0800

    nested loop
    Membuat nested loop

---
+++ day65.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class day65 {
+    
+    public static void main(String[] args) {
+        for(int h = 1; h <= 3;h++){
+        System.out.println("loop luar : " + h);
+            
+        for(int i = 1; i <= 2;i++){
+        System.out.println("  loop dalam : " + i);
+          }
+        }
+    }
+}
