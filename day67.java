b929402efb0e0577be022f1650621346a52244ac
Halik7 <khaliquzumaki25@gmail.com>
2024-11-07 23:40:45 +0800

    Method void
    Membuat method void

---
+++ day67.java
@@ -0,0 +1,15 @@
+package mentor_armawan;
+
+public class Halik {
+    
+    public void cetakPesan() {
+        System.out.println("selamat malam kak");
+    }
+    
+    public static void main(String[] args){
+        
+        Halik halik = new Halik();
+        
+        halik.cetakPesan();
+    }
+}
