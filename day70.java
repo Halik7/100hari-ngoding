2549e12223f38d7b26177a71787c132cd01dc8c2
Halik7 <khaliquzumaki25@gmail.com>
2024-11-11 00:00:07 +0800

    Method parameter
    Membuat Method parameter

---
+++ day70.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class Day70 {
+    
+    // Method dengan parameter
+    public static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int hasil = tambah(5, 3); // Memanggil method dan memberikan nilai untuk parameter
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
