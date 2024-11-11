be744af2ee05476a0517ee868db6221c02a32e96
Halik7 <khaliquzumaki25@gmail.com>
2024-11-11 21:11:41 +0800

    Method static
    Membuat method static

---
+++ Day71.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class Day71 {
+    
+    public static void tampilkanPesan() {
+        System.out.println("p mabar");
+    }
+
+    // Metode utama
+    public static void main(String[] args) {
+        // Memanggil metode statis tanpa membuat objek
+        Day71.tampilkanPesan();
+    }
+}
