ad89e4faef1d781a27d72f016913a9a9091dd1cd
Halik7 <khaliquzumaki25@gmail.com>
2024-11-13 22:50:27 +0800

    String method charAt()
    Membuat String method charAt()

---
+++ day73.java
@@ -0,0 +1,15 @@
+package mentor_armawan;
+
+public class Day73 {
+    
+    public static void main(String[] args) {
+        String nama = "khaliq";
+        char a = nama.charAt(0);
+        System.out.println("huruf pertama : " + a);
+        char b = nama.charAt(4);
+        System.out.println("huruf kelimna : "+ b);
+        char c = nama.charAt(nama.length() -1);
+        System.out.println("huruf terakhir : "+ c);
+    }
+    
+}
