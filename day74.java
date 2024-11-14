a8956e14fbd22404a3bf5e075202d8f1a152c0e6
Halik7 <khaliquzumaki25@gmail.com>
2024-11-14 20:16:25 +0800

    String method length ()
    Membuat String method length ()

---
+++ day74.java
@@ -0,0 +1,11 @@
+package mentor_armawan;
+
+public class Day74 {
+    
+    public static void main(String[] args) {
+        String nama ="khaliq ";
+        System.out.println("panjang nama : " + nama.length());
+        
+    }
+    
+}
