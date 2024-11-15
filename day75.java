c9d6be1da7aa98428b61ded3ceb9fae7e1280f69
Halik7 <khaliquzumaki25@gmail.com>
2024-11-15 21:01:49 +0800

    String method toLower case
    Membuat String method toLower case

---
+++ day75.java
@@ -0,0 +1,10 @@
+package mentor_armawan;
+
+public class Day75 {
+    
+    public static void main(String[] args) {
+        String teks = "HAlik";
+        String teksKecil = teks.toLowerCase();
+        System.out.println(teksKecil); 
+    }
+}
