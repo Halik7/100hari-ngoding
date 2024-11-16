9540e0cd1d2844d6336bc2ae153df76b4a82fdd7
Halik7 <khaliquzumaki25@gmail.com>
2024-11-16 18:14:44 +0800

    String method toUpper case()
    membuat String method toUpper case()

---
+++ day76.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class Day76 {
+    
+    public static void main(String[] args) {
+        String teks = "Halikk";
+        
+        String teksKapital = teks.toUpperCase();
+       
+        System.out.println("Teks asli: " + teks);
+        System.out.println("Teks dalam huruf kapital: " + teksKapital);
+    }
+}
