f80973649c2aa0566de848fc3d99c9e8b0dd21b8
Halik7 <khaliquzumaki25@gmail.com>
2024-11-20 21:38:20 +0800

    String method trim()
    Membuat String method trim()

---
+++ day80.java
@@ -0,0 +1,13 @@
+package mentor_armawan;
+
+public class Day80 {
+    
+    public static void main(String[] args){
+        
+        String Bifore = "   Halo, Bang  ";
+        String After = Bifore.trim();
+
+        System.out.println("Sebelum trim: '" + Bifore + "'");
+        System.out.println("Setelah trim: '" + After + "'");
+    }
+}
