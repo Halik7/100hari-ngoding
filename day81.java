1ec830968860ed9b76b05ff64752e2a5b6b4cc7c
Halik7 <khaliquzumaki25@gmail.com>
2024-11-21 20:33:40 +0800

    String method ValueOf
    Membuat String method ValueOf

---
+++ day81.java
@@ -0,0 +1,10 @@
+package mentor_armawan;
+
+public class Day81 {
+    
+    public static void main(String[] args) {
+        int angka = 100;
+        String strAngka = String.valueOf(angka);
+        System.out.println(strAngka); // Output: "100"
+    }
+}
