68768eb63562ba6507c9136cfe8e6443a8130895
Halik7 <khaliquzumaki25@gmail.com>
2024-11-22 21:17:54 +0800

    Method ceil(), floor() dan round()
    Membuat Method ceil(), floor() dan round()

---
+++ day82.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class Day82 {
+    
+    public static void main(String[] args) {
+        double angka1 = 5.7;
+        double angka2 = 5.6;
+        double angka3 = 5.5;
+        System.out.println(Math.ceil(angka1));
+        System.out.println(Math.floor(angka2));
+        System.out.println(Math.round(angka3));
+    }
+    
+}
