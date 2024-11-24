d0fd75eb38bd380f50681530f1b83559a02cdadb
Halik7 <khaliquzumaki25@gmail.com>
2024-11-24 23:18:09 +0800

    Membuat objek array satu dimensi
    Membuat array satu dimensi

---
+++ day83.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class Day83 {
+    
+    public static void main(String[] args) {
+        
+        // Mendeklarasikan dan menginisialisasi array satu dimensi string nama
+        String[] nama = {"Halik", "asep", "sumanto", "bagas"};
+
+        // Mengakses elemen dalam array
+        System.out.println(nama[0]);  // Output: Halik
+        System.out.println(nama[1]);  // Output: asep
+    }
+}
