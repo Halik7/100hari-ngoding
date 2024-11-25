96af1b98b74b7046ec42d0993da7c8be2582e1dd
Halik7 <khaliquzumaki25@gmail.com>
2024-11-25 19:53:38 +0800

    Mengisi elemen ke array satu dimensi pada saat di inisialisasi

---
+++ day85.java
@@ -0,0 +1,18 @@
+package mentor_armawan;
+
+public class Day85 {
+    
+    public static void main(String[] args) {
+       //cara pertama 
+        int angka[] = new int[5];
+        angka[0]=1;
+        angka[1]=2;
+        angka[2]=3;
+        angka[3]=4;
+        angka[4]=5;
+        
+        //cara kedua
+     int [] nomor ={1,2,3,4,5};
+    
+    }
+}
