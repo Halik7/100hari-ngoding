8f5b7c21ff09b01765212f4a5c861f828d90817c
Halik7 <khaliquzumaki25@gmail.com>
2024-11-26 20:32:06 +0800

    mencetak elemen pada array satu dimensi
    mencetak elemen pada array satu dimensi

---
+++ day86.java
@@ -0,0 +1,14 @@
+package mentor_armawan;
+
+public class Day86 {
+    
+    public static void main(String[] args) {
+        // Mendefinisikan array satu dimensi
+        int[] array = {10, 20, 30, 40, 50};
+
+        // Mencetak elemen array
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("elemen ke " + (i + 1) + ": " + array[i]);
+        }
+    }
+}
